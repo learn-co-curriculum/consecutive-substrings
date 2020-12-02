281166ce3c12c1269699abb5c07bb209b4ade5ea
Rupa <rupa211984@gmail.com>
2020-12-02 12:41:46 -0800

    Add arrows

--- javascript/consecutive_substrings.js
+++ javascript/consecutive_substrings.js
@@ -5,12 +5,12 @@ function consecutiveSubstrings(string) {
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log(consecutiveSubstrings('abc'));
+  console.log("=>", consecutiveSubstrings('abc'));
 
   console.log("");
 
   console.log("Expecting: ['a']");
-  console.log(consecutiveSubstrings('a'));
+  console.log("=>", consecutiveSubstrings('a'));
 }
 
 module.exports = consecutiveSubstrings;
--- ruby/consecutive_substrings.rb
+++ ruby/consecutive_substrings.rb
@@ -4,11 +4,13 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']"
+  print "=> " 
   print consecutive_substrings('abc')
 
   puts
 
   puts "Expecting: ['a']"
+  print "=> " 
   print consecutive_substrings('a')
 
   # Don't forget to add your own!
